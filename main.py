09e23e9a8a3506fc22909f7bd9a5b72ccfec8b69
vibe-coder-swaroop <swroopss486@gmail.com>
2026-09-12 14:42:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ os.makedirs(LOCAL_TEMP_DIR, exist_ok=True)
 
 # Massive range array targeting 946 continuous index links
 target_downloads = {
-    "Maths": [ i for i in range(5162,5262)]
+    "Maths": [ i for i in range(5330,5362)]
 }
 
 async def main():
